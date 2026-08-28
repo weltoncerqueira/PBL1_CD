2c24c94a9a710717a880352f102d2d003c1c5b4b
Welton Cerqueira <143881974+weltoncerqueira@users.noreply.github.com>
2026-08-27 22:08:38 -0300

    Refactor complementoDe2_8bits module
    Refactor complementoDe2_8bits module to simplify logic and output naming.

--- complementoDe2_8bits.v
+++ complementoDe2_8bits.v
@@ -1,24 +1,16 @@
 // Módulo do complemento de 2 de 8bits 
 
 module complementoDe2_8bits (
-	input  wire [7:0] A,
+	 input  wire [7:0] A,
     input  wire        bs,   // 1 = aplicar complemento, 0 = passar direto
-	output wire [7:0] saida
+    output wire [7:0] out
 );
-    wire [7:0] notA, carry, out;
+    wire [7:0] notA, carry;
 	 
-    // Negação das entradas para obter os valores invertidos
-    not (notA[0], A[0]);
-    not (notA[1], A[1]);
-    not (notA[2], A[2]);
-    not (notA[3], A[3]);
-    not (notA[4], A[4]); 
-    not (notA[5], A[5]);
-    not (notA[6], A[6]);
-    not (notA[7], A[7]);
+    assign notA = A ^ {8{bs}};
     
     // Somar +1 ao número invertido 
-	 somador_completo fa1 (.A(notA[0]), .B(1'b1), .cin(1'b0),     .S(out[0]), .cout(carry[0]) );
+	 somador_completo fa1 (.A(notA[0]), .B(1'b0), .cin(bs),       .S(out[0]), .cout(carry[0]) );
 	 somador_completo fa2 (.A(notA[1]), .B(1'b0), .cin(carry[0]), .S(out[1]), .cout(carry[1]) );
 	 somador_completo fa3 (.A(notA[2]), .B(1'b0), .cin(carry[1]), .S(out[2]), .cout(carry[2]) );
 	 somador_completo fa4 (.A(notA[3]), .B(1'b0), .cin(carry[2]), .S(out[3]), .cout(carry[3]) );
@@ -27,12 +19,6 @@ module complementoDe2_8bits (
 	 somador_completo fa7 (.A(notA[6]), .B(1'b0), .cin(carry[5]), .S(out[6]), .cout(carry[6]) );
 	 somador_completo fa8 (.A(notA[7]), .B(1'b0), .cin(carry[6]), .S(out[7]), .cout(carry[7]) );
 	 
-	 // Se negativo, usa complemento; senão, usa original
-    mux_2x1_8bits mux_resultado (
-        .in(A),       // entrada 0: original (positivo)
-        .notIn(out),  // entrada 1: complemento (negativo)
-        .sel(bs),     // se negativo (1), escolhe complemento
-        .out(saida)
-    );
+	 //assign saida = out;
 	 
 endmodule
